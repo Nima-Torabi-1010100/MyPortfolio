a34f4a45f564c75bd540be946997075d7c808f6b
Nima_Torabi <nimatorabi.t@gmail.com>
2024-07-21 12:12:37 +0330

    Home controller and views added.

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -1,29 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyPortfolio.Models;
-using System;
+using MyPortFolio.Models;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace MyPortfolio.Controllers
+namespace MyPortFolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        //private readonly List<Portfolio> _portfolios = new List<Portfolio>()
+        //{
+        //    new Portfolio()
+        //    {
+        //        Title="Web Page", Description="Web Projects.", Image = "Web.jpg"
+        //    },
+        //    new Portfolio()
+        //    {
+        //        Title = "Mobile App", Description = "Mobile Projects.", Image = "Mobile.jpg"
+        //    }
+        //};
+        //private readonly List<Portfolio> _projects = new List<Portfolio>()
+        //{
+        //    new Portfolio()
+        //    {
+        //        Title = "Web Page",
+        //        Description = "Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna." +
+        //        " Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.",
+        //        Image = "Web.jpg"
+        //    },
+        //    new Portfolio()
+        //    {
+        //        Title = "Application",
+        //        Description = "Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna." +
+        //        " Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.",
+        //        Image = "Mobile.jpg"
+        //    }
+        //};
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewData["headingText"] = "I'm trying to be a programmer.";
+            ViewBag.headingText = "I'm trying to be a programmer.";
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Projects()
         {
             return View();
         }
---
+++ MyPortfolio/Models/Portfolio.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortFolio.Models
+{
+    public class Portfolio
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
