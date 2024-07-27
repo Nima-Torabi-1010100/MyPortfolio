c02bc38d757a8fede28e3782eb976df9deaf3bed
Nima_Torabi <nimatorabi.t@gmail.com>
2024-07-27 11:49:57 +0330

    Server-Side form validation was implimented

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -2,32 +2,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MyPortFolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly List<Service> _services = new List<Service>()
+        {
+            new Service(1,"Golden"),
+            new Service(2,"Silver"),
+            new Service(3,"Bronze")
+        };
 
         [HttpGet]
         public IActionResult Index()
         {
             ViewData["headingText"] = "I'm trying to be a programmer.";
             ViewBag.headingText = "I'm trying to be a programmer.";
-            return View();
+            var model = new Contact()
+            {
+                Services = new SelectList(_services, "Key", "Value")
+            };
+            return View(model);
         }
 
         [HttpPost]
         public IActionResult Index(Contact model)
         {
+            model.Services = new SelectList(_services, "Key", "Value");
             if (!ModelState.IsValid)
             {
                 ViewBag.error = "The entered information is not correct, Please try again";
                 return View(model);
             }
+            ModelState.Clear();
+            model = new Contact() { Services = new SelectList(_services, "Key", "Value") };
             ViewBag.success = "Thank you! Your message has been successfully sent.";
-            return View();
-
+            return View(model);
         }
         //public IActionResult Index(Contact model)
         //{
